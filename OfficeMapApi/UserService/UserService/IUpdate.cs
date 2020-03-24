f07d7945ba258823065bc8796486e2d457fe0970
Daria-Donina <donina.daria@yandex.ru>
2020-03-24 20:58:31 +0300

    Added IUpdate and IDelete interfaces

---
+++ OfficeMapApi/UserService/UserService/IUpdate.cs
@@ -0,0 +1,33 @@
+﻿namespace UserService.Repositories.Interfaces
+{
+    /// <summary>
+    ///  Represents an interface available for updating entity.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type.</typeparam>
+    public interface IUpdate<TEntity>
+      where TEntity : class
+    {
+        /// <summary>
+        ///  Update an existing entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        TEntity Update(TEntity entity);
+    }
+
+    /// <summary>
+    ///  Represents an interface available for updating entity with some output.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type.</typeparam>
+    /// <typeparam name="TOutEntity">Out entity type.</typeparam>
+    public interface IUpdate<TEntity, TOutEntity>
+      where TEntity : class
+    {
+        /// <summary>
+        ///  Update an existing entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <param name="output">Output.</param>
+        TEntity Update(TEntity entity, out TOutEntity output);
+    }
+}
+
