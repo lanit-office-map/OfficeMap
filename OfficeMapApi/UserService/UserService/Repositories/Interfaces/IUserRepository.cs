7a8ff52f13df498e289d019b0327207085b65c4a
Daria-Donina <donina.daria@yandex.ru>
2020-03-24 21:17:38 +0300

    Added IUserRepository

---
+++ OfficeMapApi/UserService/UserService/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using UserService.Models;
+using UserService.Repositories.Filters;
+
+namespace UserService.Repositories.Interfaces
+{
+    /// <summary>
+    /// User repository interface.
+    /// </summary>
+    public interface IUserRepository : 
+        ICreate<UserRequest>,
+        IGet<User, UserFilter>,
+        IUpdate<UserRequest>,
+        IDelete<User>
+    {
+        /// <summary>
+        /// Get DB user model.
+        /// </summary>
+        object GetDbUser(UserFilter filter);
+
+        //object нужно будет заменить на DbUser
+    }
+}
