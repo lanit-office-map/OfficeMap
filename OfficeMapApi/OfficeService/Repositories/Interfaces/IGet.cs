a5fdddde82e8e473b2f9fadf45d51a99a42179b1
mekk1t <breamman@yandex.ru>
2020-03-26 23:42:33 +0300

    Interface Classes
    Описание методов

---
+++ OfficeMapApi/OfficeService/Repositories/Interfaces/IGet.cs
@@ -0,0 +1,10 @@
+﻿namespace OfficeService.Repositories.Interfaces
+{
+
+    public interface IGet<out TEntity, in TKey>
+      where TEntity : class
+    {
+        
+        TEntity Get(TKey id);
+    }
+}
---
+++ OfficeMapApi/OfficeService/Repositories/Interfaces/IUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace OfficeService.Repositories.Interfaces
+{
+
+    public interface IUpdate<TEntity>
+      where TEntity : class
+    {
+        
+        TEntity Update(TEntity entity);
+    }
+
+    
+    public interface IUpdate<TEntity, TOutEntity>
+      where TEntity : class
+    {
+        
+        TEntity Update(TEntity entity, out TOutEntity output);
+    }
+}
