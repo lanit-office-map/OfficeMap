011728d991b1a69b85dce705fb238a7b44bd4f02
mekk1t <breamman@yandex.ru>
2020-03-26 23:40:25 +0300

    Create IOfficeRepository.cs
    Added folders "Repositories", "Filters", "Interfaces"
    
    Пока что калька с ReportService, да, каюсь

---
+++ OfficeMapApi/OfficeService/Repositories/Interfaces/IOfficeRepository.cs
@@ -0,0 +1,20 @@
+﻿using OfficeService.Database.Models;
+using OfficeService.Models;
+using OfficeService.Repositories.Filters;
+
+namespace OfficeService.Repositories.Interfaces
+{
+    /// <summary>
+    /// ReportRepository interface.
+    /// </summary>
+    public interface IOfficeRepository :
+
+      IGet<Office, OfficeFilter>,
+      ICreate<OfficeRequest>,
+      IDelete<Office>,
+      IUpdate<OfficeRequest>
+    {
+
+        DbOffice GetDbReport(OfficeFilter filter);
+    }
+}
