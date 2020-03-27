941c71196106286fc03a13affe4c568f98edc460
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-03-27 12:20:32 +0300

    Merge branch 'feature/user-service' into feature/add-database-for-user-service

--- OfficeMapApi/UserService/UserService/Program.cs
+++ OfficeMapApi/UserService/UserService/Program.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using UserServiceApi;
 
-namespace UserServiceApi
+namespace UserService
 {
     public class Program
     {
---
+++ OfficeMapApi/UserService/UserService/Repositories/Interfaces/IDelete.cs
@@ -0,0 +1,15 @@
+﻿namespace UserService.Repositories.Interfaces
+{
+    /// <summary>
+    ///  Represents an interface available for deleting an entity.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type.</typeparam>
+    public interface IDelete<in TEntity>
+    {
+        /// <summary>
+        ///  Delete an entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        void Delete(TEntity entity);
+    }
+}
