c7fee0634561587fde12b30858a55e18d55d9242
mekk1t <breamman@yandex.ru>
2020-03-27 22:01:18 +0300

    Update project files

---
+++ OfficeMapApi/OfficeService/Database/DbContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace OfficeService.Database.Entities
+{
+    public partial class OfficeServiceDbContext : DbContext
+    {
+        public OfficeServiceDbContext()
+        {
+        }
+
+        public OfficeServiceDbContext(DbContextOptions<OfficeServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Offices> Offices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=DOM;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            modelBuilder.Entity<Offices>(entity =>
+            {
+                entity.HasKey(e => e.OfficeId);
+
+                entity.Property(e => e.OfficeId).ValueGeneratedNever();
+
+                entity.Property(e => e.Building)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.House)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.OfficeGuid)
+                    .HasColumnName("OfficeGUID")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasColumnName("Phone_Number")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Street)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            }); 
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ OfficeMapApi/OfficeService/Database/Entities/Offices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficeService.Database.Entities
+{
+    public partial class Offices
+    {
+        public Offices()
+        {
+            
+        }
+
+        public int OfficeId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Building { get; set; }
+        public string PhoneNumber { get; set; }
+        public Guid OfficeGuid { get; set; }
+        public bool Obsolete { get; set; }
+
+    }
+}
