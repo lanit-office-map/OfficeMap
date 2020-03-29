378120b99541e7a0dbbdb708572d2b0eb4b76d67
NadiaSob <sobo_na@mail.ru>
2020-03-29 21:01:09 +0300

    Added IFind

---
+++ OfficeMapApi/UserService/UserService/Repositories/Interfaces/IFind.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace UserService.Repositories.Interfaces
+{
+    public interface IFind
+    {
+        /// <summary>
+        /// Represents an interface available for finding entities.
+        /// </summary>
+        public interface IFind<TEntity, TFilter>
+            where TEntity : class
+        {
+            /// <summary>
+            /// Retrieve all entities, satisfying given filter.
+            /// </summary>
+            IEnumerable<TEntity> Find(TFilter filter);
+        }
+    }
+}
