33822874501084979ef7580b832ffe0ec796455a
mekk1t <breamman@yandex.ru>
2020-04-07 14:28:50 +0500

    Create OfficeRequest.cs

---
+++ OfficeMapApi/OfficeService/OfficeService/Models/OfficeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeService.Models
+{
+    public class OfficeRequest : Office
+    {
+        internal int OfficeId { get; set; }
+        internal int SpaceId { get; set; }
+    }
+}
