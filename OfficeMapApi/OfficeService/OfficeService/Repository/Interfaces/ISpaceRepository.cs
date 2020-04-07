dc1ffda86c55a5fb0681311b6e1155cc1c464704
mekk1t <breamman@yandex.ru>
2020-04-07 21:22:42 +0500

    Create ISpaceRepository.cs

---
+++ OfficeMapApi/OfficeService/OfficeService/Repository/Interfaces/ISpaceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OfficeService.Database.Entities;
+using OfficeService.Models;
+using OfficeService.Repository.Filters;
+
+namespace OfficeService.Repository.Interfaces
+{
+    public interface ISpaceRepository : IGet<Space, SpaceFilter>
+    {
+
+    }
+}
