a77ec557afa469246a6c3d7047ddbba7029a3c3d
mekk1t <breamman@yandex.ru>
2020-04-07 13:45:51 +0500

    Added Filters
    - Created OfficeFilter.cs
    - Created SpaceFilter.cs

---
+++ OfficeMapApi/OfficeService/OfficeService/Repository/Filters/OfficeFilter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeService.Repository.Filters
+{
+    public class OfficeFilter
+    {
+        public int OfficeId { get; }
+        public string City { get; }
+        public string Street { get; }
+        public string House { get; }
+        public string Building { get; }
+        public string PhoneNumber { get; }
+
+    
+        public OfficeFilter(
+            int officeId,
+            string city = null,
+            string street = null,
+            string house = null,
+            string building = null,
+            string phoneNumber = null)
+        {
+            OfficeId = officeId;
+            City = city;
+            Street = street;
+            House = house;
+            Building = building;
+            PhoneNumber = phoneNumber;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder($"(OfficeId: {OfficeId}");
+            if (City != null)
+            {
+                result.Append($", City: {City}");
+            }
+            if (Street != null)
+            {
+                result.Append($", Street: {Street}");
+            }
+            if (House != null)
+            {
+                result.Append($", House: {House}");
+            }
+            if (Building != null)
+            {
+                result.Append($", Building: {Building}");
+            }
+
+            return result.ToString();
+        }
+
+
+
+
+    }
+}
+
---
+++ OfficeMapApi/OfficeService/OfficeService/Repository/Filters/SpaceFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OfficeService.Repository.Filters
+{
+    public class SpaceFilter
+    {
+        public Guid SpaceGuid { get; }
+
+        public SpaceFilter(Guid spaceguid)
+        {
+            SpaceGuid = spaceguid;
+        }
+
+    }
+}
