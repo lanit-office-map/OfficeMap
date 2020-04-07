fd9933e10edfd959ea2dd840ecd046b2007752bc
mekk1t <breamman@yandex.ru>
2020-04-07 18:22:12 +0500

    Update IOfficeRepository.cs
    Added interfaces Create, Delete, Get, Update.

--- OfficeMapApi/OfficeService/OfficeService/Repository/Interfaces/IOfficeRepository.cs
+++ OfficeMapApi/OfficeService/OfficeService/Repository/Interfaces/IOfficeRepository.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using OfficeService.Database.Entities;
+using OfficeService.Models;
+using OfficeService.Repository.Filters;
 
 namespace OfficeService.Repository.Interfaces
 {
-  public interface IOfficeRepository
-  {
-    Task<IEnumerable<DbOffice>> GetOfficesAsync();
-  }
+    public interface IOfficeRepository :
+    
+        ICreate<OfficeRequest>,
+        IDelete<Office>,
+        IGet<Office, OfficeFilter>,
+        IUpdate<OfficeRequest>
+        
+    { 
+        Task<IEnumerable<DbOffice>> GetOfficesAsync();
+    }
 }
