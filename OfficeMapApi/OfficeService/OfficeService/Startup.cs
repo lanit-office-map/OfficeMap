665e1dc5d7a31d3815c784fd47fead9e0657259d
mekk1t <breamman@yandex.ru>
2020-04-10 22:24:12 +0500

    Update Startup.cs
    Deleted unused directives.
    Added AutoMapper via services.AddAutoMapper

--- OfficeMapApi/OfficeService/OfficeService/Startup.cs
+++ OfficeMapApi/OfficeService/OfficeService/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OfficeService.Database;
-using OfficeService.Database.Entities;
 using OfficeService.Mappers;
-using OfficeService.Mappers.Interfaces;
 using OfficeService.Repository;
 using OfficeService.Repository.Interfaces;
 using OfficeService.Services.Interface;
@@ -32,9 +27,8 @@ namespace OfficeService
         {
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<OfficeServiceDbContext>(options => options.UseSqlServer(connectionString));
-
+            services.AddAutoMapper(typeof(OfficeModelsProfile));
             services.AddScoped<IOfficeRepository, OfficeRepository>();
-            services.AddScoped<IOfficeMapper, OfficeMapper>();
             services.AddScoped<IOfficeService, Services.OfficesService>();
 
             services.AddControllers();
