0ff0fb3f1a19345a04a7b6a821a888b3cac3f23f
mekk1t <breamman@yandex.ru>
2020-04-10 22:23:36 +0500

    Create OfficeModelsProfile.cs
    THIS IS AN AUTOMAPPER
    Maps data from Office model to DbOffice and vice versa.

---
+++ OfficeMapApi/OfficeService/OfficeService/Mappers/OfficeModelsProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using OfficeService.Database.Entities;
+using OfficeService.Models;
+
+namespace OfficeService.Mappers
+{
+    public class OfficeModelsProfile : Profile
+    {
+        public OfficeModelsProfile()
+        {
+            #region Office
+
+            CreateMap<DbOffice, Office>();
+
+            CreateMap<Office, DbOffice>()
+                .ForMember(dboffice => dboffice.Spaces, opt => opt.Ignore());
+            #endregion
+        }
+    }
+}
