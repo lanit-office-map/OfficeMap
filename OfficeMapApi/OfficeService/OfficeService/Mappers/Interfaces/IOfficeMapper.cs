43d2b002235449c24ec909970099f8bf6a3bb430
mekk1t <breamman@yandex.ru>
2020-04-10 22:22:36 +0500

    Deleted mapper
    Deleted mapper, because we've configurated an automapper.

--- OfficeMapApi/OfficeService/OfficeService/Mappers/Interfaces/IOfficeMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using OfficeService.Database.Entities;
-using OfficeService.Models;
-
-namespace OfficeService.Mappers.Interfaces
-{
-  public interface IOfficeMapper : IMapper<Office, DbOffice>
-  {
-  }
-}
