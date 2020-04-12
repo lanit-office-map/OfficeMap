affc2a236ee963c1edd55dd88684a833ffaa5e0e
NadiaSob <sobo_na@mail.ru>
2020-04-12 17:09:56 +0300

    Added comments, authorize; added method GetUsers(); used mapper

--- OfficeMapApi/UserService/UserService/Controllers/UserController.cs
+++ OfficeMapApi/UserService/UserService/Controllers/UserController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UserService.Database.Entities;
+using UserService.Models;
 
 namespace UserService.Controllers
 {
@@ -22,8 +26,26 @@ namespace UserService.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Returns all users.
+        /// </summary>
+        /// <response code="200">Successfully returned users.</response>
+        [HttpGet("users")]
+        [ProducesResponseType(typeof(List<User>), 200)]
+        public IActionResult GetUsers()
+        {
+            var users = _userManager.Users.ToList();
+
+            return Ok(_mapper.Map<User[]>(users));
+        }
+
+        /// <summary>
+        /// Returns user by userGuid.
+        /// </summary>
+        /// <response code="200">Successfully returned user.</response>
+        /// <response code="404">User not found.</response>
         [HttpGet("users/{userGuid}")]
-        [ProducesResponseType(typeof(DbUser), 200)]
+        [ProducesResponseType(typeof(User), 200)]
         [ProducesResponseType(404)]
         public async Task<IActionResult> GetUser(Guid userGuid)
         {
@@ -31,16 +53,23 @@ namespace UserService.Controllers
 
             if (user != null)
             {
-                return Ok(user);
+                return Ok(_mapper.Map<User>(user));
             }
 
             return NotFound();
         }
 
+        /// <summary>
+        /// Deletes the specified user by userGuid.
+        /// </summary>
+        /// <response code="204">Successfully deleted user.</response>
+        /// <response code="503">WorkplaceService or MeetingService unavailable.</response>
+        /// <response code="404">User not found.</response>
         [HttpDelete("users/{userGuid}")]
         [ProducesResponseType(204)]
         [ProducesResponseType(503)]
         [ProducesResponseType(404)]
+        [Authorize(Policy = "IsAdmin")]
         public async Task<IActionResult> DeleteUser(Guid userGuid)
         {
             var user = await _userManager.FindByIdAsync(userGuid.ToString());
