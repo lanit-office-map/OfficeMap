94db3c55174f290431db7f5716857381b6d31bbf
Evgeny Lositsky <elosyash239@gmail.com>
2020-04-12 20:49:34 +0300

    Добавил пустые компоненты и роутинг

--- OfficeMapUI/src/app/app-routing.module.ts
+++ OfficeMapUI/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MapViewComponent } from './map-view/map-view.component';
+import { ModalBookComponent } from './modal-book/modal-book.component';
+import { ModalUserComponent } from './modal-user/modal-user.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { OfficeListComponent } from './office-list/office-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { MapEditorComponent } from './map-editor/map-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: LeftMenuComponent },
+  { path: 'map/:id', component: MapViewComponent },
+  { path: 'map/:id/book_room/:roomId', component: ModalBookComponent },
+  { path: 'map/:id/user_info/:userId', component: ModalUserComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'offices', component: OfficeListComponent },
+  { path: 'users', component: EmployeeListComponent },
+  { path: 'editor/:id', component: MapEditorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- OfficeMapUI/src/app/app.module.ts
+++ OfficeMapUI/src/app/app.module.ts
@@ -2,15 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './auth/auth.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { SearchComponent } from './search/search.component';
+import { OfficeListComponent } from './office-list/office-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { MapEditorComponent } from './map-editor/map-editor.component';
+import { ModalUserComponent } from './modal-user/modal-user.component';
+import { ModalBookComponent } from './modal-book/modal-book.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    LeftMenuComponent,
+    TopMenuComponent,
+    SearchComponent,
+    OfficeListComponent,
+    EmployeeListComponent,
+    MapViewComponent,
+    MapEditorComponent,
+    ModalUserComponent,
+    ModalBookComponent,
+    RouterModule,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
