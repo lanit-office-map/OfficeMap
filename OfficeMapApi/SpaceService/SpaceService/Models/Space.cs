753e05828afa411e9e54176ebe0abc26caaa309e
mekk1t <breamman@yandex.ru>
2020-04-15 18:05:01 +0500

    Created models
    Added "Space" and "Space Type" models for front.

---
+++ OfficeMapApi/SpaceService/SpaceService/Models/Space.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SpaceService.Models
+{
+    public class Space
+    {
+        [IgnoreDataMember]
+        public Guid SpaceGuid { get; set; }
+        public int OfficeId { get; set; }
+        public int? ParentId { get; set; }
+        public int? MapId { get; set; }
+        public int? TypeId { get; set; }
+        public string SpaceName { get; set; }
+        public string Description { get; set; }
+        public int? Capacity { get; set; }
+        public int? Floor { get; set; }
+        
+    }
+}
