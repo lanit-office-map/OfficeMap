c60bca785a5934783ba9485c40fcc7394850fef2
mekk1t <breamman@yandex.ru>
2020-04-16 16:39:26 +0500

    Update SpaceType.cs
    SpaceType.Bookable is now true or false.
    Default: false
    cannot be null.

--- OfficeMapApi/SpaceService/SpaceService/Models/SpaceType.cs
+++ OfficeMapApi/SpaceService/SpaceService/Models/SpaceType.cs
@@ -7,7 +7,7 @@ namespace SpaceService.Models
     {
         [IgnoreDataMember]
         public Guid SpaceTypeGuid { get; set; }
-        public bool? Bookable { get; set; }
+        public bool Bookable { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         
