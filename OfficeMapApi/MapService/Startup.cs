474f4d14a7a93f3c51e269f1e70c2790a7fb242a
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-04-20 19:48:10 +0300

    Init

---
+++ OfficeMapApi/MapService/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MapService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            //services.AddDbContext<MapServiceDbContext>(options => options.UseSqlServer(connectionString));
+            //services.AddAutoMapper(typeof(MapModelsProfile));
+            //services.AddScoped<IMapRepository, MapRepository>();
+            //services.AddScoped<IMapService, Services.MapService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
