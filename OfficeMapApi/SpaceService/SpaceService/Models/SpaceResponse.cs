80b929af331921380a744ed0fc6029a1046dce3d
mekk1t <breamman@yandex.ru>
2020-05-13 00:12:11 +0500

    Update SpaceResponse.cs
    Corrected an output model: added the "SpaceName" property and removed the  "OfficeGuid" property.

--- OfficeMapApi/SpaceService/SpaceService/Models/SpaceResponse.cs
+++ OfficeMapApi/SpaceService/SpaceService/Models/SpaceResponse.cs
@@ -10,11 +10,11 @@ namespace SpaceService.Models
         // GET/spaces/{spaceGuid}
 
         public Guid SpaceGuid { get; set; }
-        public Guid OfficeGuid { get; set; }
         public ICollection<SpaceResponse> Spaces { get; set; }
         public MapResponse Map { get; set; }
         public SpaceTypeResponse SpaceType { get; set; }
         public IEnumerable<WorkplaceResponse> Workplaces { get; set; }
+        public string SpaceName { get; set; }
         public int Capacity { get; set; }
         public string Description { get; set; }
         public int Floor { get; set; }
