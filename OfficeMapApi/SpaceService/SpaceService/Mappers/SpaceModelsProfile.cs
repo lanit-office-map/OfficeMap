7f84a64273882660802bc18bfe3ebedbfc701a62
mekk1t <breamman@yandex.ru>
2020-05-15 23:01:09 +0500

    Update Automapper
    Deleted useless mappings

--- OfficeMapApi/SpaceService/SpaceService/Mappers/SpaceModelsProfile.cs
+++ OfficeMapApi/SpaceService/SpaceService/Mappers/SpaceModelsProfile.cs
@@ -14,14 +14,13 @@ namespace SpaceService.Mappers
             CreateMap<DbSpaceType, SpaceType>();
 
             CreateMap<SpaceType, DbSpaceType>()
-                .ForMember(dbspacetype => dbspacetype.Spaces, opt => opt.Ignore())
                 .ForMember(dbspacetype => dbspacetype.Obsolete, opt => opt.Ignore())
                 .ForMember(dbspacetype => dbspacetype.TypeId, opt => opt.Ignore());
 
 
             CreateMap<DbSpace, Space>()
-                .ForMember(space => space.SpaceTypeGuid, opt => opt.MapFrom(src => src.SpaceTypes.SpaceTypeGuid))
-                .ForMember(space => space.Map, opt => opt.MapFrom(src => src.MapFiles));
+                .ForMember(space => space.SpaceTypeGuid, opt => opt.MapFrom(src => src.SpaceType.SpaceTypeGuid))
+                .ForMember(space => space.Map, opt => opt.MapFrom(src => src.MapFile));
             CreateMap<Space, DbSpace>()
                 .ForMember(dbspace => dbspace.Obsolete, opt => opt.Ignore())
                 .ForMember(dbspace => dbspace.SpaceId, opt => opt.Ignore());
@@ -29,25 +28,14 @@ namespace SpaceService.Mappers
 
 
             CreateMap<DbSpace, SpaceResponse>()
-                .ForMember(response => response.Spaces, opt => opt.MapFrom(src => src.InverseParent))
-                .ForMember(response => response.SpaceGuid, opt => opt.MapFrom(src => src.SpaceGuid))
-                .ForMember(response => response.OfficeGuid, opt => opt.Ignore())
-                .ForMember(response => response.Map, opt => opt.MapFrom(src => src.MapFiles))
-                .ForMember(response => response.SpaceType, opt => opt.MapFrom(src => src.SpaceTypes))
-                .ForMember(response => response.Capacity, opt => opt.MapFrom(src => src.Capacity))
-                .ForMember(response => response.Description, opt => opt.MapFrom(src => src.Description))
-                .ForMember(response => response.Floor, opt => opt.MapFrom(src => src.Floor));
-                
+                .ForMember(response => response.Map, opt => opt.MapFrom(src => src.MapFile));
 
-            CreateMap<DbSpaceType, SpaceTypeResponse>()
-                .ForMember(response => response.SpaceTypeGuid, opt => opt.MapFrom(src => src.SpaceTypeGuid))
-                .ForMember(response => response.Bookable, opt => opt.MapFrom(src => src.Bookable))
-                .ForMember(response => response.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(response => response.Description, opt => opt.MapFrom(src => src.Description));
+
+
+            CreateMap<DbSpaceType, SpaceTypeResponse>();
 
             CreateMap<DbMapFile, MapResponse>()
-                .ForMember(response => response.Content, opt => opt.MapFrom(src => Encoding.UTF8.GetString(src.Content)))
-                .ForMember(response => response.MapGuid, opt => opt.MapFrom(src => src.MapGuid));
+                .ForMember(response => response.Content, opt => opt.MapFrom(src => Encoding.UTF8.GetString(src.Content)));
 
             CreateMap<DbMapFile, Map>()
                 .ForMember(m => m.Content, opt => opt.MapFrom(src => Encoding.UTF8.GetString(src.Content)));
