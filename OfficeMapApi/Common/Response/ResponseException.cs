6e15be7a49b4a523269b01877e35afb549f85c4e
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-05-16 04:33:19 +0300

    Update Common with Response.cs

---
+++ OfficeMapApi/Common/Response/ResponseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace Common.Response
+{
+    public class ResponseException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public ResponseException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
