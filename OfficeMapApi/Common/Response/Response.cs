4969976525986294af6dee0227df3f1cdd519145
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-05-17 17:02:29 +0300

    Added new type of response

--- OfficeMapApi/Common/Response/Response.cs
+++ OfficeMapApi/Common/Response/Response.cs
@@ -7,13 +7,22 @@ namespace Common.Response
         public ResponseResult Status { get; set; }
         public T Result { get; set; }
         public ResponseException Error { get; set; }
+
         public Response(T result)
         {
             Result = result;
             Status = ResponseResult.Success;
         }
+
         public Response(HttpStatusCode statusCode, string message)
         {
+            Status = ResponseResult.Error;
+            Error = new ResponseException(statusCode, message);
+        }
+
+        public Response(T result, HttpStatusCode statusCode, string message)
+        {
+            Result = result;
             Status = ResponseResult.Error;
             Error = new ResponseException(statusCode, message);
         }
