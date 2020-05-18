a5876409748a09c58090a1cd2a9a6b6d34396c09
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-05-18 18:49:00 +0300

    Create WorkplaceRequest.cs

---
+++ OfficeMapApi/WorkplaceService/Models/Services/WorkplaceRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkplaceService.Models.Services
+{
+    public class WorkplaceRequest
+    {
+        public Guid OfficeGuid { get; set; }
+        public Guid SpaceGuid { get; set; }
+        public Guid? WorkplaceGuid { get; set; }
+        public Workplace Workplace { get; set; }
+    }
+}
