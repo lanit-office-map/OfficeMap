9c133fa53444b1cf44377e7bbd5d8a7e7d1fa3e1
mekk1t <breamman@yandex.ru>
2020-05-20 17:03:07 +0500

    Dummy WorkplaceResponse

--- OfficeMapApi/SpaceService/SpaceService/Models/WorkplaceResponse.cs
+++ OfficeMapApi/SpaceService/SpaceService/Models/WorkplaceResponse.cs
@@ -7,7 +7,11 @@ namespace SpaceService.Models
 {
     public class WorkplaceResponse
     {
+        public int WorkplaceId { get; set; }
+        public string Name { get; set; }
+        /*
         public Guid Guid { get; set; }
         public MapResponse WorkplaceMap { get; set; }
+        */
     }
 }
