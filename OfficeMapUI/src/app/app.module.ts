d1febc7021ff25ba5a2fdd8cb97d02edf3cedc16
Yuniya Kim <kim.yuniya@mail.ru>
2020-05-21 21:51:52 +0300

    Tried to fix routing

--- OfficeMapUI/src/app/app.module.ts
+++ OfficeMapUI/src/app/app.module.ts
@@ -5,32 +5,20 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AdminModule } from './home/content/admin/admin.module';
-import { MapModule } from './home/content/map/map.module';
 import { AuthorizationModule } from './authorization/authorization.module';
-import { HomeComponent } from './home/home.component';
-import { ContentModule } from './home/content/content.module';
-import { NavigationModule } from './home/navigation/navigation.module';
 import { HomeModule } from './home/home.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LeftMenuComponent } from './home/navigation/left-menu/left-menu.component';
-import { TopMenuComponent } from './home/navigation/top-menu/top-menu.component';
-import { SettingsComponent } from './home/navigation/settings/settings.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftMenuComponent,
-    TopMenuComponent,
-    SettingsComponent,
-    NotFoundComponent,
-    HomeComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -39,14 +27,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
-    AdminModule,
-    MapModule,
     AuthorizationModule,
     MatExpansionModule,
     MatToolbarModule,
     MatSidenavModule,
-    ContentModule,
-    NavigationModule,
     HomeModule
   ],
   providers: [],
--- OfficeMapUI/src/app/home/home.module.ts
+++ OfficeMapUI/src/app/home/home.module.ts
@@ -5,9 +5,13 @@ import { HomeRoutingModule } from './home-routing.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { ContentModule } from './content/content.module';
 
+import { HomeComponent } from './home.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule,
